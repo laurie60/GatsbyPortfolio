34a8c5152e5bed08e7b0a63ea1b1b7be92440e40
laurie60 <laurie8000@googlemail.com>
2022-11-11 12:09:31 +0000

    set up tags

--- src/components/TagsList.jsx
+++ src/components/TagsList.jsx
@@ -1,9 +1,25 @@
+import { Link } from "gatsby"
 import React from "react"
 import setupTags from "../utils/setupTech"
+
 const TagsList = ({ projects }) => {
   const newTags = setupTags(projects)
-  console.log(newTags, "should be alpha")
-  return <div>this is tagslist</div>
+
+  return (
+    <div className="tag-container">
+      <h4>Projects</h4>
+      <div className="tags-list">
+        {newTags.map((tag, index) => {
+          const [text, value] = tag
+          return (
+            <Link to={`/${text}`} key={index}>
+              {text} ({value})
+            </Link>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default TagsList
--- src/pages/tags.jsx
+++ src/pages/tags.jsx
@@ -1,14 +1,41 @@
+import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
+import setupTags from "../utils/setupTech"
 
-const Tags = () => {
+const Tags = ({
+  data: {
+    allContentfulProject: { nodes: projects },
+  },
+}) => {
+  const niceTags = setupTags(projects)
+  console.log(niceTags)
+  console.log(projects)
   return (
     <Layout>
       <div>
         <h1>Hello From Tags Page</h1>
+        {niceTags.map(tag => {
+          const [text, number] = tag
+          return (
+            <p>
+              {text} ({number})
+            </p>
+          )
+        })}
       </div>
     </Layout>
   )
 }
 
 export default Tags
+
+export const query = graphql`
+  query {
+    allContentfulProject {
+      nodes {
+        technologies
+      }
+    }
+  }
+`
