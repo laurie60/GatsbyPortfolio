18167fd0357de0b86af2a3a4fc096f9f1eb015ca
laurie60 <laurie8000@googlemail.com>
2022-11-21 17:32:06 +0000

    add cache of fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,7 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-image`,
+    `gatsby-plugin-preload-fonts`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
